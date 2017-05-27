6fdf0296528ea145e508a82cfa312960fe7a9cc6
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-05-26 20:56:39 -0400

    gprimelte: Commonise audio configuration files

--- device.mk
+++ device.mk
@@ -11,14 +11,3 @@ LOCAL_PATH := device/samsung/gprimeltespr
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gprimeltespr/overlay
-
-# Audio configuration
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/audio/mixer_paths.xml:system/blobs/etc/mixer_paths.xml
-
-# Media configurations
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/media/media_codecs_8929.xml:system/etc/media_codecs_8929.xml \
-	$(LOCAL_PATH)/media/media_codecs_8939.xml:system/etc/media_codecs_8939.xml \
-	$(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
