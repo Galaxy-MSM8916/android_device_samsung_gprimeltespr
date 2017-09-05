5fcf321c27057a7f8f4d367956c1d2e8974a1f66
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-09-05 07:55:04 -0400

    gprimeltespr: Move system properties to system.prop
    Change-Id: I29128aa9b248038536c52dc697a8fe30feb856bc

--- device.mk
+++ device.mk
@@ -6,9 +6,6 @@ $(call inherit-product, device/samsung/gprimelte-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/gprimeltespr
 
-# System properties
--include $(LOCAL_PATH)/system_prop.mk
-
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gprimeltespr/overlay
 
