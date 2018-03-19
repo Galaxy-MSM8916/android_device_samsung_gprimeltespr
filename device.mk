47935eebd06fa3c37acf277b40d5360eab0c8c7d
Zvikomborero Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-03-19 16:06:16 -0400

    Revert "gprimeltespr: Move system properties to system.prop"
    This reverts commit 5fcf321c27057a7f8f4d367956c1d2e8974a1f66.
    
    Change-Id: I13b1b6be447f595aa57301ca5bf5063dc6d7ff03

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@ $(call inherit-product, device/samsung/gprimelte-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/gprimeltespr
 
+# System properties
+-include $(LOCAL_PATH)/system_prop.mk
+
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gprimeltespr/overlay
 
